00de1dfb5876267b2d8d12652673f744a04fbcc7
David Belzunces Candel <davidbelzunces6@gmail.com>
2021-02-16 20:02:48 +0100

    Ejercicio 7

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -11,10 +11,10 @@
           <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Migraciones</a>
+                <a class="nav-link" href="Migraciones">Migraciones</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Seeders</a>
---
+++ resources/views/migraciones.blade.php
@@ -0,0 +1,11 @@
+@include('head')
+@include('header')
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">¡La tabla se ha creado correctamente!</h1>
+    <p class="lead"></p>
+  </div>
+</main>
+
+@include('footer')
