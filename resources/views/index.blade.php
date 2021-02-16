2ab407b908ba039aee92d779f599ab24d48a2985
David Belzunces Candel <davidbelzunces6@gmail.com>
2021-02-16 20:44:02 +0100

    Ejercicio 11

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @include('head')
 @include('header')
 <!-- Begin page content -->
-<main class="flex-shrink-0">
+<main class="flex-shrink-0" style="background-image: url('10.jpg');">
   <div class="container">
     <h1 class="mt-5">¡Bienvenido Miquel!</h1>
     <p class="lead">Hola, ¿que tal, como estas? Espero que bien, por que viendo este trabajo seguro que es asi.</p>
